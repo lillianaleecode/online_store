13fa950688d59d06ce577868e21bebbede095c0c
lillianaleecode <lilliana.lee@code.berlin>
2022-03-18 11:16:04 +0100

    set the response as a dictionary to have the name along

--- products_dao.py
+++ products_dao.py
@@ -7,16 +7,29 @@ def get_all_products():
 
     cursor = cnx.cursor() #documentation from https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html
 
-    query = "SELECT products.product_id, products.name, products.price_per_unit, unit_measure.unit_measure_name FROM online_store.products JOIN online_store.unit_measure ON products.unit_measure_id = unit_measure.unit_measure_id ;"
+    query = "SELECT products.product_id, products.name, unit_measure.unit_measure_name, products.price_per_unit FROM online_store.products JOIN online_store.unit_measure ON products.unit_measure_id = unit_measure.unit_measure_id ;" #order matters
 
     cursor.execute(query)
 
-    
+    response = []
 
     for (product_id, name, unit_measure_id, price_per_unit) in cursor:
-        print(product_id, name, unit_measure_id, price_per_unit)
+        response.append(
+            {
+                'product_id' : product_id,
+                'name': name,
+                'price_per_unit' : price_per_unit,
+                'unit_measure_id' : unit_measure_id,
+                
+                #'unit_measure_name' : unit_measure_name,
+
+            }
+        )
+        
+        #print(product_id, name, unit_measure_id, price_per_unit)
 
     cnx.close()
+    return response
 
 if __name__=='__main__':
     print(get_all_products())
