5fce82c863ce7030babc79ee48938c3f8b241ce2
lillianaleecode <lilliana.lee@code.berlin>
2022-03-18 10:52:13 +0100

    Add Query and using inner Join Tables

--- products_dao.py
+++ products_dao.py
@@ -2,6 +2,15 @@ import mysql.connector
 
 cnx= mysql.connector.connect(user='root', password='ORACLE545901', host='127.0.0.1', database='online_store')
 
-cnx.close()
 
-#cursor = cnx.cursor() #documentation from https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html
+
+cursor = cnx.cursor() #documentation from https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html
+
+query = "SELECT products.product_id, products.name, products.price_per_unit, unit_measure.unit_measure_name FROM online_store.products JOIN online_store.unit_measure ON products.unit_measure_id = unit_measure.unit_measure_id ;"
+
+cursor.execute(query)
+
+for (product_id, name, unit_measure_id, price_per_unit) in cursor:
+    print(product_id, name, unit_measure_id, price_per_unit)
+
+cnx.close()
