1af62705160b222951b7c3f8bfc3af473920a911
lillianaleecode <lilliana.lee@code.berlin>
2022-03-31 15:47:16 +0200

    function to delete row in table

--- products_dao.py
+++ products_dao.py
@@ -45,13 +45,23 @@ def insert_new_product(connection, product):
     connection.commit()
     return cursor.lastrowid
 
+def delete_product(connection, product):
+    cursor = connection.cursor()
+    query = ("DELETE FROM online_store.products where product_id=" + str(product)) 
+
+    cursor.execute(query)
+    connection.commit()
+    return cursor.lastrowid
+
 if __name__=='__main__':
     connection = get_sql_connection()
     print(get_all_products(connection))
 
     print(insert_new_product(connection, {
-        'product_id': 7,
+        'product_id': 9,
         'product_name': 'crepes',
         'unit_measure_id': '1',
         'price_per_unit': 10
     }))
+
+    print(delete_product(connection,7))
