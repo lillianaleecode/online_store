db1167fdaa57cb646a86b5deea9be087b7f01a13
lillianaleecode <lilliana.lee@code.berlin>
2022-04-01 16:56:08 +0200

    get products in the page

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, jsonify
+from sql_connection import get_sql_connection
+
+
+import products_dao
+
+connection = get_sql_connection() 
 
 app = Flask(__name__)
 
 @app.route("/")
 
-def hello_world():
-    return "hello Lilly to my website! nananana 123456"
+
+def get_products():
+    response = products_dao.get_all_products(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == "__main__":
     print("starting python Flask")
